0ed4e8a557bed7c7e84ddc4d6419cb85ef5b6e2a
dhanJi_exp <kdhanji183@gmail.com>
2025-08-13 15:05:23 +0530

    add cookies option

--- src/config/cookie.config.js
+++ src/config/cookie.config.js
@@ -2,5 +2,5 @@ export const cookieOptions = {
   httpOnly: true, // Prevents JavaScript access to the cookie
   secure: process.env.NODE_ENV === "production", // Ensures the cookie is sent over HTTPS
   maxAge: 24 * 60 * 60 * 1000, // 1 day
-  sameSite: "lax", // Prevents CSRF attacks by ensuring the cookie is sent only for same-site requests
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Prevents CSRF attacks by ensuring the cookie is sent only for same-site requests
 };
